5f11acc10b471eb5e0220fc78c5cf65abdb08c33
kuro-water <kuro07001@gmail.com>
2024-02-19 00:35:04 +0900

    preloadのconst/exposeInMainWorldを修正

--- src/preload.ts
+++ src/preload.ts
@@ -31,21 +31,6 @@ contextBridge.exposeInMainWorld("electronAPI", {
 const CONFIG_PATH = "\\config.json";
 contextBridge.exposeInMainWorld("CONFIG_PATH", CONFIG_PATH);
 
-const I_MINO = 0;
-contextBridge.exposeInMainWorld("I_MINO", I_MINO);
-const T_MINO = 1;
-contextBridge.exposeInMainWorld("T_MINO", T_MINO);
-const O_MINO = 2;
-contextBridge.exposeInMainWorld("O_MINO", O_MINO);
-const L_MINO = 3;
-contextBridge.exposeInMainWorld("L_MINO", L_MINO);
-const J_MINO = 4;
-contextBridge.exposeInMainWorld("J_MINO", J_MINO);
-const S_MINO = 5;
-contextBridge.exposeInMainWorld("S_MINO", S_MINO);
-const Z_MINO = 6;
-contextBridge.exposeInMainWorld("Z_MINO", Z_MINO);
-
 const INIT_FIELD = [
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -104,113 +89,6 @@ contextBridge.exposeInMainWorld("DRAW_FIELD_WITDH", DRAW_FIELD_WITDH);
 const DRAW_FIELD_LEFT = 1;
 contextBridge.exposeInMainWorld("DRAW_FIELD_LEFT", DRAW_FIELD_LEFT);
 
-// 参考：https://tetris.wiki/Super_Rotation_System
-// 画像を見ながら座標をベタ打ちした。こうでないとSRSの動作が難しい
-// How Guideline move Really Works
-// prettier-ignore
-const MINO_POS = [
-    [ // I
-        [[-1, 0], [0, 0], [1, 0], [2, 0]],
-        [[1, -1], [1, 0], [1, 1], [1, 2]],
-        [[-1, 1], [0, 1], [1, 1], [2, 1]],
-        [[0, -1], [0, 0], [0, 1], [0, 2]],
-    ],
-    [ // T
-        [[0, -1], [-1, 0], [0, 0], [1, 0]],
-        [[0, -1], [0, 0], [1, 0], [0, 1]],
-        [[-1, 0], [0, 0], [1, 0], [0, 1]],
-        [[0, -1], [-1, 0], [0, 0], [0, 1]],
-    ],
-    [ // O
-        [[0, -1], [1, -1], [0, 0], [1, 0]],
-        [[1, -1], [0, 0], [1, 0], [0, -1]],
-        [[0, 0], [1, 0], [0, -1], [1, -1]],
-        [[1, 0], [0, -1], [1, -1], [0, 0]],
-    ],
-    [ // L
-        [[1, -1], [-1, 0], [0, 0], [1, 0]],
-        [[0, -1], [0, 0], [0, 1], [1, 1]],
-        [[-1, 0], [0, 0], [1, 0], [-1, 1]],
-        [[-1, -1], [0, -1], [0, 0], [0, 1]],
-    ],
-    [ // J
-        [[-1, -1], [-1, 0], [0, 0], [1, 0]],
-        [[0, -1], [1, -1], [0, 0], [0, 1]],
-        [[-1, 0], [0, 0], [1, 0], [1, 1]],
-        [[0, -1], [0, 0], [-1, 1], [0, 1]],
-    ],
-    [ // S
-        [[0, -1], [1, -1], [-1, 0], [0, 0]],
-        [[0, -1], [0, 0], [1, 0], [1, 1]],
-        [[0, 0], [1, 0], [-1, 1], [0, 1]],
-        [[-1, -1], [-1, 0], [0, 0], [0, 1]],
-    ],
-    [ // Z
-        [[-1, -1], [0, -1], [0, 0], [1, 0]],
-        [[1, -1], [0, 0], [1, 0], [0, 1]],
-        [[-1, 0], [0, 0], [0, 1], [1, 1]],
-        [[0, -1], [-1, 0], [0, 0], [-1, 1]],
-    ],
-];
-contextBridge.exposeInMainWorld("MINO_POS", MINO_POS);
-
-// prettier-ignore
-const SRS_TLJSZ = [
-    [
-        [],
-        [[-1, 0], [-1, -1], [0, 2], [-1, 2]],
-        [],
-        [[1, 0], [1, -1], [0, 2], [1, 2]],
-    ],
-    [
-        [[1, 0], [1, 1], [0, -2], [1, -2]],
-        [],
-        [[1, 0], [1, 1], [0, -2], [1, -2]],
-        [],
-    ],
-    [
-        [],
-        [[-1, 0], [-1, -1], [0, 2], [-1, 2]],
-        [],
-        [[1, 0], [1, -1], [0, 2], [1, 2]],
-    ],
-    [
-        [[-1, 0], [-1, 1], [0, -2], [-1, -2]],
-        [],
-        [[-1, 0], [-1, 1], [0, -2], [-1, -2]],
-        [],
-    ],
-];
-contextBridge.exposeInMainWorld("SRS_TLJSZ", SRS_TLJSZ);
-// prettier-ignore
-const SRS_I = [
-    [
-        [],
-        [[-2, 0], [1, 0], [-2, 1], [1, -2]],
-        [],
-        [[-1, 0], [2, 0], [-1, -2], [2, 1]],
-    ],
-    [
-        [[2, 0], [-1, 0], [2, -1], [-1, 2]],
-        [],
-        [[-1, 0], [2, 0], [-1, -2], [2, 1]],
-        [],
-    ],
-    [
-        [],
-        [[1, 0], [-2, 0], [1, 2], [-2, -1]],
-        [],
-        [[2, 0], [-1, 0], [2, -1], [-1, 2]],
-    ],
-    [
-        [[1, 0], [-2, 0], [1, 2], [-2, -1]],
-        [],
-        [[-2, 0], [1, 0], [-2, 1], [1, -2]],
-        [],
-    ],
-];
-contextBridge.exposeInMainWorld("SRS_I", SRS_I);
-
 const MINO_COLORS = ["#0F9BD7", "#AF298A", "#E39F02", "#E35B02", "#2141C6", "#59B101", "#D70F37"];
 contextBridge.exposeInMainWorld("MINO_COLORS", MINO_COLORS);
 const GHOST_COLORS = ["#074D6B", "#571445", "#714F01", "#712D01", "#102063", "#2C5800", "#6B071B"];
@@ -244,9 +122,6 @@ const SET_DELAY = 20; // 接地硬直
 contextBridge.exposeInMainWorld("SET_DELAY", SET_DELAY);
 const DEL_DELAY = 100; // ライン消去時の硬直
 contextBridge.exposeInMainWorld("DEL_DELAY", DEL_DELAY);
-
-const KSKS_LIMIT = 12;
-contextBridge.exposeInMainWorld("KSKS_LIMIT", KSKS_LIMIT);
 
 const INIT_KEY_MAP = {
     moveLeft: "ArrowLeft",
