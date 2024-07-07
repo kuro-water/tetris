45c2a14bbca6a363a3078a2f67e2c4c5b46f2583
kuro-water <kuro07001@gmail.com>
2024-07-07 16:30:12 +0900

    [refactor].d.ts内がごちゃごちゃしてたので、preloadで公開するための型定義はpreload.d.ts, その他（本来の意味での）型定義はdeclare.d.tsに記述するように

--- src/@types/preload.d.ts
+++ src/@types/preload.d.ts
@@ -17,33 +17,3 @@ declare class wetris {
     static hold(idx: number): null;
     static getField(idx: number): Promise<number[][]>;
 }
-
-type KeyMap = {
-    moveLeft: string;
-    moveRight: string;
-    softDrop: string;
-    hardDrop: string;
-    rotateLeft: string;
-    rotateRight: string;
-    hold: string;
-};
-type Config = {
-    keyMode: string;
-    keyMap: KeyMap;
-};
-
-type position = {
-    x: number;
-    y: number;
-};
-
-// Tuple<number, number>のとき、number[]を返す(number extends N == trueとなる)
-// それ以外のとき、_TupleOfを呼び出す
-type Tuple<T, N extends number> = number extends N ? T[] : _TupleOf<T, N, []>;
-// (R["length"] === N)になるまで再帰でタプルを作成
-type _TupleOf<T, N extends number, R extends unknown[]> = R["length"] extends N
-    ? R
-    : _TupleOf<T, N, [T, ...R]>;
-
-type row = Tuple<number, 12>;
-type field = Tuple<row, 41>;
